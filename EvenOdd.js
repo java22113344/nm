b00d47269d683365fdaed8d03c063b7b8db56396
java22113344 <ganeshgrl005@gmail.com>
2025-04-07 22:03:34 +0530

    Create EvenOdd.js

---
+++ EvenOdd.js
@@ -0,0 +1,11 @@
+<script>
+  function checkEvenOrOdd(number) { 
+    if (number % 2 === 0) { 
+        console.log(number + " is Even"); 
+    } else { 
+        console.log(number + " is Odd"); 
+    } 
+} 
+checkEvenOrOdd(5);  
+checkEvenOrOdd(8);  
+</script>
