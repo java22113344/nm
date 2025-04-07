bacc018a59e4022a229ab29128107098eb80ee52
java22113344 <ganeshgrl005@gmail.com>
2025-04-07 22:00:57 +0530

    Create ChangeHeading.js

---
+++ ChangeHeading.js
@@ -0,0 +1,8 @@
+<h1 id="heading">Welcome!</h1> 
+<button onclick="changeText()">Change Text</button> 
+ 
+<script> 
+function changeText() { 
+    document.getElementById('heading').innerText = 'Hello, World!'; 
+} 
+</script> 
